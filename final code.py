58a92f5315375cf27314f3fa5fbba80a64edd7b9
lwatson430 <89879383+lwatson430@users.noreply.github.com>
2021-09-28 14:17:58 -0500

    Update final code.py
    Commenting out lines 10-13.

--- final code.py
+++ final code.py
@@ -7,10 +7,10 @@ LOCAL_FILE = 'local_copy.log'
 local_file, headers = urlretrieve(URL_PATH, LOCAL_FILE)
 
 # Alt.: supply an anonmymous callback function to print a simple progress bar to screen
-local_file, headers = urlretrieve(URL_PATH, LOCAL_FILE, lambda x,y,z: print('.', end='', flush=True))
+#local_file, headers = urlretrieve(URL_PATH, LOCAL_FILE, lambda x,y,z: print('.', end='', flush=True))
 
 # Alt. 2: a progress bar with reduced output (every 1000 blocks)
-local_file, headers = urlretrieve(URL_PATH, LOCAL_FILE, lambda x,y,z: print('.', end='', flush=True) if x % 100 == 0 else False)
+#local_file, headers = urlretrieve(URL_PATH, LOCAL_FILE, lambda x,y,z: print('.', end='', flush=True) if x % 100 == 0 else False)
 
 FILE_NAME = 'local_copy.log'
 
