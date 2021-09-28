a4a267b8bdd0cc1f0d8d54a4273ec3224b4e8d0c
mray-99 <89879060+mray-99@users.noreply.github.com>
2021-09-28 10:36:14 -0500

    Update read_file.py
    Reads the log file and produces total number of requests

--- read_file.py
+++ read_file.py
@@ -1,21 +1,16 @@
-FILE_NAME = 'path/to/file'
+from urllib.request import urlretrieve
 
-# Use open() to get a filehandle that can access the file
+URL_PATH = 'https://s3.amazonaws.com/tcmg476/http_access_log'
+LOCAL_FILE = 'local_copy.log'
+
+local_file, headers = urlretrieve(URL_PATH, LOCAL_FILE)
+
+FILE_NAME = 'local_copy.log'
+
 fh = open(FILE_NAME)
 
-# Loop through the file 
 for line in fh:
-  print(line)
+ print(line)
 
-# The filehandle object provides many basic file operations:
-fh.read(64)     # read the specified bytes from the file (if no byte size is specified, then the entire file is read)
-fh.readline()   # read a single line from the file (up to a newline character - \n)
-fh.write('some data here')  # write a string to the file (make sure the file was open()'ed for writing!)
-fh.close()      # close the file when you're finished with it
-
-  
-# Alternately, skip the assignment to the filehandle altogether:
-for line in open(FILE_NAME):
-  print(line)
-
-# The loop example above is memory-efficient, and also easy to read
+num_lines = sum(1 for line in open('local_copy.log'))
+print('The total number of requests made is' , num_lines)
