ad770503ae6bc6cd87c004211d9de9a2d89ac327
nbaker00 <89879388+nbaker00@users.noreply.github.com>
2021-10-06 18:44:10 -0500

    Update final code.py

--- final code.py
+++ final code.py
@@ -40,9 +40,15 @@ for line in fh:
 print("There have been", counter, "total requests!")
 print ("There have been", counter2, "requests in the last 6 months!")
 
-#pull request test
 
 # \[(\d{2})/([A-Za-z]{3,4})/(\d{4}):(\d{2}:\d{2}:\d{2}).+\] \"([A-Z]{3,6}) (.+) HTTP/1.0\" (\d{3}) .*
 
+fh2 = open(FILE_NAME)
+
+
+import calendar
+import datetime
+from datetime import datetime
+from datetime import date
 import re
 regex = re.compile('\[(\d{2})/([A-Za-z]{3,4})/(\d{4}):(\d{2}:\d{2}:\d{2}).+\] \"([A-Z]{3,6}) (.+) HTTP/1.0\" (\d{3}) .*')
